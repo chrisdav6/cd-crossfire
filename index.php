7f8792d3699ce4d1268b46e18ea04ce061717eec
Chris Davis <chris.davis5440@gmail.com>
2018-10-25 10:34:07 -0400

    added meta description

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="<?php echo "CD Crossfire is an elite softball organization located in New York's Capital District"; ?>">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,600,700,900" rel="stylesheet">
     <!-- Font Awesome -->
@@ -16,14 +17,14 @@
     <link rel="stylesheet" href="public/css/bootstrap.css" type="text/css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="public/css/style.css">
-    <title><?php echo $title ?></title>
+    <title><?php echo $title; ?></title>
   </head>
 
   <body class="d-flex flex-column">
     <?php include_once "includes/navbar.php"?>
 
     <div class="container py-5">
-      <h1><?php echo $title ?></h1>
+      <h1><?php echo $title; ?></h1>
     </div>
     
     <?php include_once "includes/footer.php" ?>
